d3d6fac91ce85902b2bbf9caff601c6c414e192b
yxuchen <yaoxuecheng@xiaohuazhu.com>
2019-08-13 20:54:36 +0800

    feat: reactApplication middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,74 +1,14 @@
-import fs from "fs"
 import Koa from "koa"
 import Statics from "koa-static"
 
-import React from "react"
-import { renderToString } from "react-dom/server"
-import { StaticRouter, matchPath } from "react-router-dom"
-
 import paths from "../../webpack/paths"
-import routes from "~/routes"
-import App from "~/App"
+import reactApplication from '~/server/middlewares/reactApplication'
 
 const app = new Koa()
 
 app.use(Statics(paths.resolveRoot("dist")))
 
-app.use(ctx => {
-  const context = {}
-  const { url, path } = ctx
-  const currentRoute = routes.find(route => matchPath(path, route))
-
-  if (!currentRoute) {
-    ctx.status = 404
-    return
-  }
-
-  const { name: spanName } = currentRoute
-
-  const assetsMapStr = fs.readFileSync(
-    paths.resolveRoot("dist", "client", "manifest.json"),
-    "utf-8"
-  )
-  const cssRegExp = /\.css$/
-  const jsRegExp = /\.js$/
-  const assetsValues = Object.values(JSON.parse(assetsMapStr))
-  const styles = assetsValues.filter(
-    assetsPath =>
-      cssRegExp.test(assetsPath) && assetsPath.startsWith(`client/${spanName}.`)
-  )
-  const scripts = assetsValues.filter(
-    assetsPath =>
-      jsRegExp.test(assetsPath) && assetsPath.startsWith(`client/${spanName}.`)
-  )
-
-  const stylesStr = styles
-    .map(style => `<link href=${style} rel="stylesheet"></link>`)
-    .join("")
-  const scriptsStr = scripts
-    .map(js => `<script src=${js} async></script>`)
-    .join("")
-
-  const markup = renderToString(
-    <StaticRouter location={url} context={context}>
-      <App />
-    </StaticRouter>
-  )
-
-  ctx.body = `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>SSR with RR</title>
-        ${stylesStr}
-      </head>
-      <body>
-        <div id="app">${markup}</div>
-        ${scriptsStr}
-      </body>
-    </html>
-  `
-})
+app.use(reactApplication)
 
 const port = process.env.DEV_PORT || 8083
 
