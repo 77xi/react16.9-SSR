5d37bd0a12bd273655d2a8645015ec96851f150e
yxuchen <yaoxuecheng@xiaohuazhu.com>
2019-08-14 17:45:24 +0800

    feat: add doc for getAssets

--- src/server/util/getAssets.js
+++ src/server/util/getAssets.js
@@ -1,12 +1,10 @@
-// match home.hash.js
-const regExp = /^home\..*\.js$/
-
 const assetsClientPath = "client"
 
 const getAssets = ({ spanName, assetsMapStr }) => {
   const assetsPath = Object.values(JSON.parse(assetsMapStr))
   const rootPath = `${assetsClientPath}\/${spanName}`
-  
+
+  // match dist/client/home.hash.js
   const jsRegExp = new RegExp(`^${rootPath}\\..*\\.js$`)
   const cssRegExp = new RegExp(`^${rootPath}\\..*\\.css$`)
 
