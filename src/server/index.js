44bab96a7eb9a6a5fc3f30821b94898c9cd19751
yxuchen <yaoxuecheng@xiaohuazhu.com>
2019-08-14 17:44:15 +0800

    chore: createServer js

--- src/server/index.js
+++ src/server/index.js
@@ -6,13 +6,17 @@ import paths from "~/../webpack/paths"
 import reactApplication from "~/server/middlewares/reactApplication"
 import apiErrorHandler from "~/server/middlewares/apiErrorHandler"
 
-const app = new Koa()
-
-app.use(Statics(paths.resolveRoot("dist")))
-
-app.use(apiErrorHandler)
-app.use(reactApplication)
-
 const port = process.env.DEV_PORT || 8083
 
-app.listen(port, () => console.log(`web server listening ${port}`))
+const createServer = port => {
+  const app = new Koa()
+
+  app.use(Statics(paths.resolveRoot("dist")))
+
+  app.use(apiErrorHandler)
+  app.use(reactApplication)
+
+  app.listen(port, () => console.log(`web server listening ${port}`))
+}
+
+createServer(port)
